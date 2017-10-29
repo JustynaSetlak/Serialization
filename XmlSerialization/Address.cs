3ce6e82f6ee0dbbb00a8760dc44adb100e4f0479
jsetlak@pgs-soft.com <justa96.s1@gmail.com>
2017-10-29 19:38:35 +0100

    xml serialization

---
+++ XmlSerialization/Address.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace XmlSerialization
+{
+    public class Address
+    {
+        [XmlIgnore]
+        public int Id { get; set; }
+        [XmlAttribute("NazwaUlicy")]
+        public string StreetName { get; set; }
+        [XmlAttribute("NumerDomu")]
+        public int HouseNumber { get; set; }
+        [XmlAttribute("Miasto")]
+        public string City { get; set; }
+    }
+}
---
+++ XmlSerialization/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XmlSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = CreatePerson();
+
+            using (Stream stream = File.Create("person.xml"))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+                xmlSerializer.Serialize(stream,person);
+            }
+
+            Person personFromFile;
+            using (Stream stream = File.OpenRead("person.xml"))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+                personFromFile = (Person)xmlSerializer.Deserialize(stream);
+            }
+            Console.WriteLine(personFromFile.FirstName + " " + personFromFile.LastName);
+            Console.ReadKey();
+        }
+
+        static Person CreatePerson()
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                DateOfBirth = new DateTime(1993, 02, 01),
+                Address = new Address
+                {
+                    Id = 1,
+                    City = "Wrocław",
+                    HouseNumber = 10,
+                    StreetName = "Polaka"
+                }
+            };
+        }
+    }
+}
